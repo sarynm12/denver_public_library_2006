391e0fe652c9dafdb16b51e41c474262c60ac40c
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-22 12:51:11 -0600

    write method working

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,20 @@
 class Author
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :books
 
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
+    @books = []
   end
 
   def name
     @first_name + " " + @last_name
+  end
+
+  def write(title, publication_date)
+    new_book = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: title, publication_date: publication_date})
+    @books << new_book
+    new_book 
   end
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -12,7 +12,19 @@ class AuthorTest < Minitest::Test
 
   def test_it_has_a_name
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    assert_equal "Charlotte Bronte", charlotte_bronte.name 
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+  end
+
+  def test_it_starts_with_no_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    assert_equal [], charlotte_bronte.books
+  end
+
+  def test_it_can_write_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title 
   end
 
 end
@@ -22,23 +34,7 @@ end
 # Use TDD to create an `Author` class that responds to the following interaction pattern.
 #
 # The `write` method must take two Strings as arguments and return an instance of `Book`.
-#
-# ```ruby
-# pry(main)> require './lib/book'
-# #=> true
-#
-# pry(main)> require './lib/author'
-# #=> true
-#
-# #=> #<Author:0x00007fb898081850...>
-#
-# pry(main)> charlotte_bronte.name
-# #=> "Charlotte Bronte"
-#
-# pry(main)> charlotte_bronte.books
-# #=> []
-#
-# pry(main)> jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
 # #=> #<Book:0x00007fb896e22538...>
 #
 # pry(main)> jane_eyre.class
